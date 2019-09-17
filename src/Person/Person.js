8c77f81c82db03b2c558b128ac5e7b33d35d0230
Gabriel Borba <gabriel.lewis@acad.pucrs.br>
2019-09-16 23:31:42 -0300

    First commit;
    First commit;

---
+++ src/Person/Person.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Person.css'
+const person  = (props) => {
+     return (
+         <div className={"Person"}>
+             <p onClick={props.click}>Iam a {props.name} and  iam {props.age}</p>
+             <p>{props.children}</p>
+             <input type="text" onChange={props.changed} value={props.name} />
+         </div>
+     )
+};
+export default person;
